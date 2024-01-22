06f490f20fc68fadff4be297ac0ee2d5d035db95
Abubakar Javed <shykhabubakar99@gmail.com>
2024-01-22 21:32:26 +0500

    Chat

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -9,49 +10,82 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return  DefaultTabController(
-        length: 4, child: Scaffold(
-      appBar: AppBar(
-        title: const Text('WhatsApp', style: TextStyle(color: Colors.white),),
-        backgroundColor: Colors.teal,
-        bottom: const TabBar(
-          tabs: [
-            Tab(child: Icon(Icons.camera_alt_sharp, color: Colors.white,)),
-            Text('Chat', style: TextStyle(color: Colors.white),),
-            Text('Status', style: TextStyle(color: Colors.white),),
-            Text('Calls', style: TextStyle(color: Colors.white),),
-          ],
-
-        ),
-        actions: [
-         const  Icon(Icons.search, color: Colors.white,),
-          PopupMenuButton(
-            iconColor: Colors.white,
-              itemBuilder: (context)=> [
-               const  PopupMenuItem(
-                    child:Text('New Group'),
-                ),
-                const PopupMenuItem(
-                  child:Text('Setings'),
-                ),
-                const PopupMenuItem(
-                  child:Text('Channel'),
-                ),
-
-              ]
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'WhatsApp',
+            style: TextStyle(color: Colors.white),
           ),
-        ],
+          backgroundColor: Colors.teal,
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                  child: Icon(
+                Icons.camera_alt_sharp,
+                color: Colors.white,
+              )),
+              Text(
+                'Chat',
+                style: TextStyle(color: Colors.white),
+              ),
+              Text(
+                'Status',
+                style: TextStyle(color: Colors.white),
+              ),
+              Text(
+                'Calls',
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
+          actions: [
+            const Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            const SizedBox(width: 20),
+            const Icon(
+              Icons.message,
+              color: Colors.white,
+            ),
+            PopupMenuButton(
+                iconColor: Colors.white,
+                itemBuilder: (context) => [
+                      const PopupMenuItem(
+                        child: Text('New Group'),
+                      ),
+                      const PopupMenuItem(
+                        child: Text('Setings'),
+                      ),
+                      const PopupMenuItem(
+                        child: Text('Channel'),
+                      ),
+                    ]),
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            Text('Camera'),
+            ListView.builder(
+                itemCount: 1000,
+                itemBuilder: (context, index) {
+                  return const ListTile(
+                    title: Text('John Steve'),
+                    subtitle: Text('How are you doing?'),
+                    trailing: Text('9:31 PM'),
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+                    ),
+                  );
+                }),
+            Text('Status'),
+            Text('Calls'),
+          ],
+        ),
       ),
-      body: const TabBarView(
-        children: [
-          Text('Camera'),
-          Text('Chat'),
-          Text('Status'),
-          Text('Calls'),
-
-        ],
-      ),
-    ),
     );
   }
 }
