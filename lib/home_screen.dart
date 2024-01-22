421b7864af2b3b8d77d7a2e843cb67cd50fba8ff
Abubakar Javed <shykhabubakar99@gmail.com>
2024-01-22 21:24:51 +0500

    Popup Menu

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,16 +12,44 @@ class _HomeScreenState extends State<HomeScreen> {
     return  DefaultTabController(
         length: 4, child: Scaffold(
       appBar: AppBar(
-        title: Text('WhatsApp', style: TextStyle(color: Colors.white),),
+        title: const Text('WhatsApp', style: TextStyle(color: Colors.white),),
         backgroundColor: Colors.teal,
-        bottom: TabBar(
+        bottom: const TabBar(
           tabs: [
+            Tab(child: Icon(Icons.camera_alt_sharp, color: Colors.white,)),
             Text('Chat', style: TextStyle(color: Colors.white),),
             Text('Status', style: TextStyle(color: Colors.white),),
             Text('Calls', style: TextStyle(color: Colors.white),),
           ],
 
         ),
+        actions: [
+         const  Icon(Icons.search, color: Colors.white,),
+          PopupMenuButton(
+            iconColor: Colors.white,
+              itemBuilder: (context)=> [
+               const  PopupMenuItem(
+                    child:Text('New Group'),
+                ),
+                const PopupMenuItem(
+                  child:Text('Setings'),
+                ),
+                const PopupMenuItem(
+                  child:Text('Channel'),
+                ),
+
+              ]
+          ),
+        ],
+      ),
+      body: const TabBarView(
+        children: [
+          Text('Camera'),
+          Text('Chat'),
+          Text('Status'),
+          Text('Calls'),
+
+        ],
       ),
     ),
     );
